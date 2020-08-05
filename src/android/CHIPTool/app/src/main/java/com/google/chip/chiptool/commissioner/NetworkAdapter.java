ed665367e4feb31fa19def48dddeae4f3881c4ac
kangping <wgtdkp@google.com>
2020-08-05 19:20:17 +0800

    add license header

--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/NetworkAdapter.java
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/NetworkAdapter.java
@@ -1,3 +1,21 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package com.google.chip.chiptool.commissioner;
 
 import android.content.Context;
@@ -11,53 +29,6 @@ import java.util.Arrays;
 import java.util.Vector;
 
 public class NetworkAdapter extends BaseAdapter {
-  /*
-  private static  final NetworkInfo[] MOCK_NETWORKS = {
-          new NetworkInfo(
-                  new BorderAgentInfo("bed room",
-                          new byte[] {0xde},
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-          new NetworkInfo(
-                  new BorderAgentInfo("living room",
-                          "0xdeadface",
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-          new NetworkInfo(
-                  new BorderAgentInfo("bath room",
-                          "0xdeadface",
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-          new NetworkInfo(
-                  new BorderAgentInfo("second floor",
-                          "0xdeadface",
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-          new NetworkInfo(
-                  new BorderAgentInfo("third floor",
-                          "0xdeadface",
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-          new NetworkInfo(
-                  new BorderAgentInfo("forth floor",
-                          "0xdeadface",
-                          "127.0.0.1",
-                          49191,
-                          "3aa55f91ca47d1e4e71a08cb35e91591"
-                  )),
-  };
-  */
-
   private Vector<NetworkInfo> networks;
 
   private LayoutInflater inflater;
--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/SelectNetworkFragment.java
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/SelectNetworkFragment.java
@@ -1,3 +1,21 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package com.google.chip.chiptool.commissioner;
 
 import android.os.Bundle;
@@ -60,7 +78,8 @@ public class SelectNetworkFragment extends Fragment {
     addDeviceButton = view.findViewById(R.id.add_device_button);
     addDeviceButton.setVisibility(View.GONE);
 
-    deviceInfo = ((CommissionerActivity)getActivity()).getDeviceInfo();
+    deviceInfo = getArguments().getParcelable(Constants.KEY_DEVICE_INFO);
+
     String deviceInfoString = String.format("version: %d\nvendorId: %d\nproductId: %d\nsetupPinCode: %d",
             deviceInfo.getVersion(), deviceInfo.getVendorId(), deviceInfo.getProductId(), deviceInfo.getSetupPinCode());
     TextView deviceInfoView = view.findViewById(R.id.device_info);
